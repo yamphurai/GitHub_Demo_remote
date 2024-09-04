1da29f3721de25f0382694bcab4983022c204288
Jeevan Rai <yamphurai007@yahoo.com>
2024-09-03 21:33:26 -0500

    Created second view

---
+++ GitHub_Demo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitHub_Demo
+//
+//  Created by Reshma Shrestha on 9/3/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
