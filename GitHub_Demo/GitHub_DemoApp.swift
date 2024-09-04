f48a18c384649b8fe51d4eeccbde93fadb9eff63
Jeevan Rai <yamphurai007@yahoo.com>
2024-09-03 21:24:22 -0500

    Initial Commit

---
+++ GitHub_Demo/GitHub_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_DemoApp.swift
+//  GitHub_Demo
+//
+//  Created by Reshma Shrestha on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
